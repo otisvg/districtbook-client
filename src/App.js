11e89662d9936d9b4dc1d3e1f49e07856b918878
Meryl Rastall <merylrastall@gmail.com>
2020-11-24 15:12:25 +0000

    feat: updated the app.js file to have an additional function - componentDidMount which sends a request to an api and stores the resulting data as an item on the state.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-export default App;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false,
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    items: json,
+                })
+            });
+    }
+
+    render() {
+
+        var { isLoaded, items } = this.state;
+
+        if (!isLoaded) {
+            return <div > Loading..... < /div>;
+        } else {
+            return (
+
+                <
+                div className = "App" >
+                Data has been loaded <
+                /div>
+            );
+        }
+    }
+
+    export default App;
